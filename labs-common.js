cd276f1e69bd881e2c25c2860c124fb412077ee6
stefanocudini <stefano.cudini@gmail.com>
2021-07-28 19:41:02 +0200

    logo

--- labs-common.js
+++ labs-common.js
@@ -13,12 +13,14 @@
 	div.innerHTML = 'The new opensource <br>geosocial platform';
 })();
 */
+/*window.GA = 'UA-157805814-1';
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-38393469-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', GA]);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();*/
