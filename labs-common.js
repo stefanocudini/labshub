679dd28328d085134021010e5b94e3fd8d69fce4
stefanocudini <stefano.cudini@gmail.com>
2021-08-21 14:48:17 +0200

    ga

--- labs-common.js
+++ labs-common.js
@@ -13,7 +13,7 @@
 	div.innerHTML = 'The new opensource <br>geosocial platform';
 })();
 */
-/*window.GA = 'UA-157805814-1';
+window.GA = 'UA-157805814-1';
 
  var _gaq = _gaq || [];
   _gaq.push(['_setAccount', GA]);
@@ -23,4 +23,5 @@
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();*/
+  })();
+//*/
